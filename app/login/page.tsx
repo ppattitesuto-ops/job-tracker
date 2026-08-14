a0226497a7ab59d43c10c25880b5e8872e871c0d
Akira iida <ppattitesuto@gmail.com>
2026-08-14 17:10:31 +0900

    feat: Google 認証を追加
    ポップアップ方式を採用。キャンセル時にエラーを出さないよう、エラーコードの日本語かを lib/authErrors.tsに切り出した。

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,9 +3,10 @@
 // 下にあるReactはReact.SubmitEventを使うのに必要、export defaultなので{}の中にない
 import React, { useEffect, useState } from "react";
 import { auth } from "@/lib/firebase";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/contexts/AuthContext";
+import { getAuthErrorMessage } from "@/lib/authErrors";
 
 export default function LoginPage() {
 
@@ -17,10 +18,10 @@ export default function LoginPage() {
   const [error, setError] = useState("");
 
   // 分割代入：useAuthは{user, loading}というオブジェクトを返す、だからこの形でキーを指定して値を代入してる
-  const {user, loading} = useAuth()
+  const { user, loading } = useAuth()
   // useRouterはページ移動を操作するためのオブジェクトを返す
   const router = useRouter();
-// ※フックは必ずトップレベルに、じゃないと状態の順番が変わる
+  // ※フックは必ずトップレベルに、じゃないと状態の順番が変わる
 
 
   useEffect(() => {
@@ -28,9 +29,9 @@ export default function LoginPage() {
       // これにより今のURLをapp/page.tsxに変える、replaceはURLを変えてそのURLに対応するページを表示する。
       router.replace("/");
     }
-  },[user, loading, router])
+  }, [user, loading, router])
 
-  
+
   const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
     setError("");
@@ -40,8 +41,21 @@ export default function LoginPage() {
       } else {
         await createUserWithEmailAndPassword(auth, email, password)
       }
-    } catch {
-      setError("ログインに失敗しました");
+    } catch(err) {
+      setError(getAuthErrorMessage(err));
+    }
+  }
+
+  const handleGoogleSignIn = async () => {
+    setError("");
+    // ここでのGoogleAuthProvider(クラス：設計図)の役割は宛先の指定、つまり認証の本人であるということをGoogleに聞きにいくということをしている。あくまで必要な情報を渡すための設定にすぎない。providerは他の権限も追加できるためこのような設定のような役割になってる。newによって設計図からインスタンスを一つ作っている。※変数に入れないと消えるから入れる
+    const provider = new GoogleAuthProvider();
+    try {
+      // signInWithPopupにproviderは認証できる場所の位置を教える。
+      await signInWithPopup(auth, provider)
+    } catch(err) {
+      // getAuthErrorMessage(err)で実行された返り値がsetErrorにセットされる。
+      setError(getAuthErrorMessage(err));
     }
   }
 
@@ -50,6 +64,7 @@ export default function LoginPage() {
       <div>ログイン画面</div>
       {/* 条件付きレンダリング（＆＆）でエラーがあるなら出す、ないなら出さない、errorが空文字だからそのまま画面に出して何も表示されないけど、０などのならそのまま表示されるから気を付ける→真偽値への変更など*/}
       {error && <div>{error}</div>}
+      <button type="button" onClick={handleGoogleSignIn}>Googleでログイン</button>
       <div>
         <label htmlFor="email">メールアドレス</label>
         <input
